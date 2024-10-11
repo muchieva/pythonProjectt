b1b7dc29e8f429c0f4bc99332acf58411ea48ef5
muchieva <muchina.ieva@gmail.com>
2024-10-11 08:56:43 +0300

    6 uzduotis

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+from itertools import count
 from random import random, randint
 
 vardas = 'Ieva'
@@ -9,6 +10,7 @@ print("As esu", vardas, pavarde,"man yra", siemetai - gimmetai , "metai")
 
 a = randint(0,4)
 b = randint(0,4)
+print(a, b)
 
 if a > b:
     print(round( a / b , 2))
@@ -19,6 +21,7 @@ x = randint(0,25)
 y = randint(0,25)
 z = randint (0,25)
 
+print(x, y, z)
 if y <= x <= z or z <= x <= y:
     print(x)
 elif x <= y <= z or z <= y <= x:
@@ -30,3 +33,81 @@ k1 = randint(1,10)
 k2 = randint(1,10)
 k3 = randint(1,10)
 
+print(k1, k2, k3)
+
+if k1**2 == k2**2 + k3**2 or k2**2 == k1**2 + k3**2 or k3**2 == k1**2 + k2**2:
+    print("statusis trikampis")
+elif k1**2 < k2**2 + k3**2 or k2**2 < k1**2 + k3**2 or k3**2 < k1**2 + k2**2:
+    print("smailusis trikampis")
+elif k1**2 > k2**2 + k3**2 and k1 < k2 + k3 or k2**2 > k1**2 + k3**2  and k2 < k1 + k3 or k3**2 > k1**2 + k2**2 and k3 < k1 + k2:
+    print("bukasis trikampis")
+else:
+    print("trikampis nesigauna")
+
+
+
+p1 = randint(0,2)
+p2 = randint(0,2)
+p3 = randint(0,2)
+p4 = randint(0,2)
+
+print(p1, p2, p3, p4)
+
+nuliai = 0
+vienetai = 1
+dvejetai = 2
+
+if p1 == 0:
+    print(nuliai)
+if p2 == 0:
+    print(nuliai)
+if p3 == 0:
+    print(nuliai)
+if p4 == 0:
+    print(nuliai)
+
+
+if p1 == 1:
+    print(vienetai)
+if p2 == 1:
+    print(vienetai)
+if p3 == 1:
+    print(vienetai)
+if p4 == 1:
+    print(vienetai)
+
+
+if p1 == 2:
+    print(dvejetai)
+if p2 == 2:
+    print(dvejetai)
+if p3 == 2:
+    print(dvejetai)
+if p4 == 2:
+    print(dvejetai)
+
+
+o1 = randint(-10,10)
+o2 = randint(-10,10)
+o3 = randint(-10,10)
+
+if o1 > 0:
+    print("[", o1, "]")
+elif o1 == 0:
+    print("(", o1 , ")")
+elif o1 < 0:
+    print("{", o1 , "}")
+
+if o2 > 0:
+    print("[", o2, "]")
+elif o2 == 0:
+    print("(", o2, ")")
+elif o2 < 0:
+    print("{", o2 , "}")
+
+if o3 > 0:
+    print("[", o3, "]")
+elif o3 == 0:
+    print("(", o3 , ")")
+elif o3 < 0:
+    print("{", o3 , "}")
